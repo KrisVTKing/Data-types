171b9afe4257c92e4736942de657dcf56e93f0ea
KrisVTKing <kriskvo.kk@gmail.com>
2024-03-15 10:09:15 +0100

    first commit

---
+++ #Data Types.py
@@ -0,0 +1,46 @@
+#Data Types
+
+#1. Integer(Int)
+# 43,87,100
+
+#x = True
+#print(x)
+
+
+#2. String "str"
+#"43""krishna"
+
+#3. FLoat
+#1.22 13.23
+
+#4. Boolean
+# True or False
+
+#5. Long
+# 207690L
+
+# Function (Block of code that performs a specific task)
+#def my_function(x):
+    #return 34 + x
+
+#print(my_function(35))
+
+#Array
+#cars = ["Ford", "Volvo", "BMW"]
+#print(type(cars))   
+
+#List
+#cars = ["Ford", "Volvo", "BMW"]
+#print(cars[2])
+
+#Immutability
+
+#cars[2] = "Toyota"
+#cars.append("Honda")
+#print(cars)
+
+#Loop used when you want to iterate over a given number of times
+names = "krishna", "kumar", "rajesh"
+for name in names :
+    newName = name.capitalize()
+    print(newName)
